170c2782b88534026957f003b9539ee15baf74d4
Souvikjana572 <147611627+Souvikjana572@users.noreply.github.com>
2024-08-04 19:56:37 +0530

    Create Evantual Safe State.cpp

---
+++ Evantual Safe State.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+   bool dfs(int node, vector<int> graph[], set<int>& s, vector<bool>& visited) {
+        if (s.find(node) != s.end())
+            return 1;
+        if (visited[node])
+            return 0;
+        visited[node] = 1;
+        bool ans = 1;
+        for (int i = 0; i < graph[node].size(); i++) {
+            ans = ans && dfs(graph[node][i], graph, s, visited);
+        }
+        if (ans) {
+            s.insert(node);
+            return 1;
+        }
+        return 0;
+    }
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+         int n =V;
+        vector<bool> visited(n, 0);
+        set<int> s;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, s, visited);
+            }
+        }
+        vector<int> ans;
+         for(auto x:s){
+            ans.push_back(x);
+        }
+        return ans;
+    }
+    
+};
